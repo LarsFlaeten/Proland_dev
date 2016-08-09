d72456cebf1783fd7b7e254218639293c74264d5
LarsFlaeten <lars.flaeten@gmail.com>
2016-08-10 00:21:45 +0200

    Change to GLFW window

--- forest/examples/trees1/HelloWorld.cpp
+++ forest/examples/trees1/HelloWorld.cpp
@@ -46,7 +46,7 @@
 #include "ork/resource/XMLResourceLoader.h"
 #include "ork/scenegraph/SceneManager.h"
 #include "ork/scenegraph/ShowLogTask.h"
-#include "ork/ui/GlutWindow.h"
+#include "ork/ui/GlfwWindow.h"
 
 #include "proland/TerrainPlugin.h"
 #include "proland/ForestPlugin.h"
@@ -57,7 +57,7 @@
 using namespace ork;
 using namespace proland;
 
-class HelloWorld : public GlutWindow, public ViewManager
+class HelloWorld : public GlfwWindow, public ViewManager
 {
 public:
     ptr<SceneManager> manager;
@@ -65,7 +65,7 @@ public:
     ptr<BasicViewHandler> view;
     ptr<EventHandler> ui;
 
-    HelloWorld() : GlutWindow(Window::Parameters().size(1024, 768))
+    HelloWorld() : GlfwWindow(Window::Parameters().size(1024, 768))
     {
         FileLogger::File *out = new FileLogger::File("log.html");
         Logger::INFO_LOGGER = new FileLogger("INFO", out, Logger::INFO_LOGGER);
@@ -101,7 +101,7 @@ public:
     virtual void redisplay(double t, double dt)
     {
         ui->redisplay(t, dt);
-        GlutWindow::redisplay(t, dt);
+        GlfwWindow::redisplay(t, dt);
 
         if (Logger::ERROR_LOGGER != NULL) {
             Logger::ERROR_LOGGER->flush();
@@ -114,13 +114,13 @@ public:
         fb->setDepthTest(true, LESS);
 		fb->setViewport(vec4<GLint>(0, 0, x, y));
         ui->reshape(x, y);
-        GlutWindow::reshape(x, y);
+        GlfwWindow::reshape(x, y);
         idle(false);
     }
 
     virtual void idle(bool damaged)
     {
-        GlutWindow::idle(damaged);
+        GlfwWindow::idle(damaged);
         if (damaged) {
             updateResources();
         }
